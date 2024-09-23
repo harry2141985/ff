2bdc9d6569f3ab992ffed6c425725f95185d5544
harry2141985 <125636784+harry2141985@users.noreply.github.com>
2024-09-23 13:56:14 +0500

    Update test_temp_helper.py

--- tests/test_temp_helper.py
+++ tests/test_temp_helper.py
@@ -3,9 +3,9 @@ import tempfile
 
 import pytest
 
-from facefusion import state_manager
-from facefusion.download import conditional_download
-from facefusion.temp_helper import get_temp_directory_path, get_temp_file_path, get_temp_frames_pattern
+from ff import state_manager
+from ff.download import conditional_download
+from ff.temp_helper import get_temp_directory_path, get_temp_file_path, get_temp_frames_pattern
 from .helper import get_test_example_file, get_test_examples_directory
 
 
@@ -20,14 +20,14 @@ def before_all() -> None:
 
 def test_get_temp_file_path() -> None:
 	temp_directory = tempfile.gettempdir()
-	assert get_temp_file_path(get_test_example_file('target-240p.mp4')) == os.path.join(temp_directory, 'facefusion', 'target-240p', 'temp.mp4')
+	assert get_temp_file_path(get_test_example_file('target-240p.mp4')) == os.path.join(temp_directory, 'ff', 'target-240p', 'temp.mp4')
 
 
 def test_get_temp_directory_path() -> None:
 	temp_directory = tempfile.gettempdir()
-	assert get_temp_directory_path(get_test_example_file('target-240p.mp4')) == os.path.join(temp_directory, 'facefusion', 'target-240p')
+	assert get_temp_directory_path(get_test_example_file('target-240p.mp4')) == os.path.join(temp_directory, 'ff', 'target-240p')
 
 
 def test_get_temp_frames_pattern() -> None:
 	temp_directory = tempfile.gettempdir()
-	assert get_temp_frames_pattern(get_test_example_file('target-240p.mp4'), '%04d') == os.path.join(temp_directory, 'facefusion', 'target-240p', '%04d.png')
+	assert get_temp_frames_pattern(get_test_example_file('target-240p.mp4'), '%04d') == os.path.join(temp_directory, 'ff', 'target-240p', '%04d.png')
